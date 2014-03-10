ab2e7791e84242b98568a43fcbf66465be68cb07
FreshXOpenSource <support@freshx.de>
2014-03-10 15:16:36 +0100

    emit attachment event

--- index.js
+++ index.js
@@ -75,6 +75,9 @@ function parseUnread() {
         parser.on("end", function(mail) {
           self.emit('mail',mail);
         });
+        parser.on('attachment', function(attachment) {
+          self.emit('attachment', attachment)
+        })
         msg.on('body', function(stream, info) {
           stream.pipe(parser);
         });
